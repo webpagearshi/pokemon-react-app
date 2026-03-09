63d8598925f87e855644154ab7413b1caa6fb8df
Arshi Saleh <arshisaleh@gmail.com>
2026-03-09 23:04:57 +0530

    Add border and shadow to card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,26 @@
+const typeColors = {
+  Fire: "#f08030",
+  Water: "#6890f0",
+  Grass: "#78c850",
+  Electric: "#f8d030",
+  Psychic: "#f85888",
+  Normal: "#a8a878",
+  Ghost: "#705898",
+  Dragon: "#7038f8",
+  Fighting: "#c03028",
+  Rock: "#b8a038",
+};
+
 function Card({ pokemon }) {
+  const color = typeColors[pokemon.type] || "#ccc";
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{
+        border: `3px solid ${color}`,
+        boxShadow: `0 6px 12px ${color}40`,
+      }}
+    >
       <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
         alt={pokemon.name}
